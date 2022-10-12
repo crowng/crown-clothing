3d483c4a3f7c6d271c63702d7419951cb5322dd6
Gabriel Corona <coronagjk@gmail.com>
2022-10-12 11:07:44 -0700

    added cart conttext and shopping icon with droopdown

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
+import App from './App';
 import './index.scss';
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -11,7 +13,11 @@ root.render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<UserProvider>
-				<App />
+				<ProductsProvider>
+					<CartProvider>
+						<App />
+					</CartProvider>
+				</ProductsProvider>
 			</UserProvider>
 		</BrowserRouter>
 	</React.StrictMode>
